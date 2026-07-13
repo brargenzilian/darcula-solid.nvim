d5f9e79794c09aed26343d56dec6980c29d44d23
brargenzilian <brargenzilian@noreply.codeberg.org>
2026-07-13 23:42:05 +0200

    Add treesitter support (#24)
    This pull request implements some basic highlight groups for treesitter support.
    
    Reviewed-on: https://codeberg.org/brargenzilian/darcula-solid.nvim/pulls/24

--- lua/darcula-solid/groups.lua
+++ lua/darcula-solid/groups.lua
@@ -192,52 +192,56 @@ return lush(function(injected_functions)
     -- specially handled to be valid lua code, we do this via the special
     -- sym function.
     --
-    -- sym"@text.literal"      { }, -- Comment
-    -- sym"@text.reference"    { }, -- Identifier
-    -- sym"@text.title"        { }, -- Title
-    -- sym"@text.uri"          { }, -- Underlined
-    -- sym"@text.underline"    { }, -- Underlined
-    -- sym"@text.todo"         { }, -- Todo
-    -- sym"@comment"           { }, -- Comment
-    -- sym"@punctuation"       { }, -- Delimiter
-    -- sym"@constant"          { }, -- Constant
-    -- sym"@constant.builtin"  { }, -- Special
-    -- sym"@constant.macro"    { }, -- Define
-    -- sym"@define"            { }, -- Define
-    -- sym"@macro"             { }, -- Macro
-    -- sym"@string"            { }, -- String
-    -- sym"@string.escape"     { }, -- SpecialChar
-    -- sym"@string.special"    { }, -- SpecialChar
-    -- sym"@character"         { }, -- Character
-    -- sym"@character.special" { }, -- SpecialChar
-    -- sym"@number"            { }, -- Number
-    -- sym"@boolean"           { }, -- Boolean
-    -- sym"@float"             { }, -- Float
-    -- sym"@function"          { }, -- Function
-    -- sym"@function.builtin"  { }, -- Special
-    -- sym"@function.macro"    { }, -- Macro
-    -- sym"@parameter"         { }, -- Identifier
-    -- sym"@method"            { }, -- Function
-    -- sym"@field"             { }, -- Identifier
-    -- sym"@property"          { }, -- Identifier
-    -- sym"@constructor"       { }, -- Special
-    -- sym"@conditional"       { }, -- Conditional
-    -- sym"@repeat"            { }, -- Repeat
-    -- sym"@label"             { }, -- Label
-    -- sym"@operator"          { }, -- Operator
-    -- sym"@keyword"           { }, -- Keyword
-    -- sym"@exception"         { }, -- Exception
-    -- sym"@variable"          { }, -- Identifier
-    -- sym"@type"              { }, -- Type
-    -- sym"@type.definition"   { }, -- Typedef
-    -- sym"@storageclass"      { }, -- StorageClass
-    -- sym"@structure"         { }, -- Structure
-    -- sym"@namespace"         { }, -- Identifier
-    -- sym"@include"           { }, -- Include
-    -- sym"@preproc"           { }, -- PreProc
-    -- sym"@debug"             { }, -- Debug
-    -- sym"@tag"               { }, -- Tag
-
+    sym("@text.literal") { Comment },
+    sym("@text.reference") { Identifier },
+    sym("@text.title") { Title },
+    sym("@text.uri") { Underlined },
+    sym("@text.underline") { Underlined },
+    sym("@text.todo") { Todo },
+    sym("@comment") { Comment },
+    sym("@punctuation") { Delimiter },
+    sym("@constant") { Constant },
+    sym("@constant.builtin") { Special },
+    sym("@constant.macro") { Define },
+    sym("@define") { Define },
+    sym("@macro") { Macro },
+    sym("@string") { String },
+    sym("@string.escape") { SpecialChar },
+    sym("@string.special") { SpecialChar },
+    sym("@character") { Character },
+    sym("@character.special") { SpecialChar },
+    sym("@number") { Number },
+    sym("@boolean") { Boolean },
+    sym("@float") { Float },
+    sym("@function") { Function },
+    sym("@function.builtin") { Special },
+    sym("@function.macro") { Macro },
+    sym("@parameter") { Identifier },
+    sym("@method") { Function },
+    sym("@field") { Identifier },
+    sym("@property") { Identifier },
+    sym("@constructor") { Special },
+    sym("@conditional") { Conditional },
+    sym("@repeat") { Repeat },
+    sym("@label") { Label },
+    sym("@operator") { Operator },
+    sym("@keyword") { Keyword, gui = 'bold' },
+    sym("@exception") { Exception },
+    sym("@variable") { Identifier },
+    sym("@variable.parameter") { Identifier },
+    sym("@variable.member") { Identifier },
+    sym("@variable.builtin") { Keyword },
+    sym("@variable.parameter.builtin") { Keyword },
+    sym("@type") { Type },
+    sym("@type.definition") { Typedef },
+    sym("@storageclass") { StorageClass },
+    sym("@structure") { Structure },
+    sym("@namespace") { Identifier },
+    sym("@include") { Include },
+    sym("@preproc") { PreProc },
+    sym("@debug") { Debug },
+    sym("@tag") { Tag },
+    sym("@tag.builtin") { Tag },
 
     -- Other highlight groups
     gitcommitSummary { fg = palette.subtle_white, bg = palette.black },
